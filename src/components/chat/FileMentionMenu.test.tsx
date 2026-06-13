ff8becd2b22913397d640ea84447365649a03e22
Arkaydeus <arktechdev+ark1@gmail.com>
2026-06-13 17:36:14 +0100

    test(chat): cover right-click guard in file mention menu
    - Assert onSelect is not called on non-left mouse down

--- src/components/chat/FileMentionMenu.test.tsx
+++ src/components/chat/FileMentionMenu.test.tsx
@@ -32,6 +32,16 @@ describe("FileMentionMenu", () => {
     expect(onSelect).toHaveBeenCalledWith(files[0]);
   });
 
+  test("ignores non-left mouse down (e.g. right click)", () => {
+    const onSelect = mock(() => {});
+
+    render(<FileMentionMenu files={files} selectedIndex={0} onSelect={onSelect} onClose={() => {}} />);
+
+    fireEvent.mouseDown(screen.getByRole("option", { name: /Button.tsx/ }), { button: 2 });
+
+    expect(onSelect).not.toHaveBeenCalled();
+  });
+
   test("supports keyboard-originated button activation", () => {
     const onSelect = mock(() => {});
 
